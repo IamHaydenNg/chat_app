9b15aa6059723a39fb774d15aa0e2fb5a0aa3401
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-12-09 11:25:50 +0800

    Class 277: Firebase CLI & SDK Setup 2/2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:chat_app/screens/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+// firebase_option automatically create by firebase done by flutterfire configure
+import 'firebase_options.dart'
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
