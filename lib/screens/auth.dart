f3c49fe6a721642d2aebb90bb5cefc279cf22c3a
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-12-10 17:53:30 +0800

    Class 290: Sending Data to Firestore

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat_app/widgets/user_image_picler.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -55,7 +56,15 @@ class _AuthScreenState extends State<AuthScreen> {
 
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
-        print(imageUrl);
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid)
+            .set({
+          'username': '',
+          'email': _enteredEmail,
+          'image_url': imageUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       //When using try {...} on... catch, only exceptions of the provided type (FirebaseAuthException in this case) will be caught & handled;
